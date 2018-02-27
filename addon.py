761cb4b2047efa4833829043565044a58ae09f56
Isayso <35309607+Isayso@users.noreply.github.com>
2018-02-27 16:24:57 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -11,5 +11,5 @@ line1 = "start resilio sync"
 xbmcgui.Dialog().ok(addonname, line1)
 #os.system()
 path = os.getcwd()[:-1]+"\\"
-# call a different script with the argument 'Hello World'
+# call script resiliostart.sh
 xbmc.executebuiltin("XBMC.RunScript("+path+"resiliostart.sh)")
